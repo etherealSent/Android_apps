d8878f943145183ad51c87f0e1b20e1bec2f4d5e
godnesty <ethereal.sent@gmail.com>
2024-08-01 12:12:27 +0700

    nav work

--- app/src/main/java/com/example/exercise202/MainActivity.kt
+++ app/src/main/java/com/example/exercise202/MainActivity.kt
@@ -12,8 +12,6 @@ import androidx.fragment.app.FragmentContainerView
 
 const val STAR_SIGN_ID = "STAR_SIGN_ID"
 
-
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
