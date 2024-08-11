5121f729580259e6eb6c993daa85d9b05d44a26a
godnesty <ethereal.sent@gmail.com>
2024-08-11 15:38:59 +0700

    done

---
+++ app/src/main/java/com/example/exercise202/PreferenceViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.exercise202
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class PreferenceViewModel(private val preferenceWrapper: PreferenceWrapper) : ViewModel() {
+
+    fun saveText(text: String) {
+        preferenceWrapper.saveText(text)
+    }
+
+    fun getText(): LiveData<String> {
+        return preferenceWrapper.getText()
+    }
+
+}
